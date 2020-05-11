a0f179be4a1a333bae7290d4dc7addfa52cc32dc
Joshua Hunt <jelijahhunt@gmail.com>
2020-05-11 20:02:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+var array = []
+for (var i = 0; i < musicians.length; i++) {
+  array.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+return array
+}
+
+function johnLennonFacts(facts){
+  var array = []
+  var num = 0
+  while(num<facts.length){
+    array.push(`${facts[num]}!!!`)
+    num++
+  } return array
+}
+
+function iLoveTheBeatles(num){
+var array = []
+do{array.push('I love the Beatles!')}
+while(num<=8)
+return array
+}
